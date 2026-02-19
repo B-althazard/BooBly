88beb49b4f14a14a102e2f6e4ac0755f29cd3346
B-althazard <hb.mcmxc@gmail.com>
2026-02-19 18:13:44 +0100

    v1.2.1

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const STORAGE = {
   page: "boobly.page",
 };
 
-const VERSION = "1.2.0";
+const VERSION = "1.2.1";
 
 const toast = (m) => {
   const t = document.createElement("div");
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='boobly-v1.2';
+const CACHE='boobly-v1.2.1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./database.json','./prompts.json','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/favicon.png','./icons/preset.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))));
