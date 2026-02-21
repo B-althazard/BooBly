e917942e14557597536aaece6a426361db7047f3
B-althazard <hb.mcmxc@gmail.com>
2026-02-21 21:50:54 +0100

    BooBly webapp_v1_4_4_Alpha_backlog1_fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// BooBly Service Worker (v1_4_3_Alpha)
-const VERSION = 'v1_4_3_Alpha';
+// BooBly Service Worker (v1_4_4_Alpha)
+const VERSION = 'v1_4_4_Alpha';
 const STATIC_CACHE = `boobly-static-${VERSION}`;
 const RUNTIME_CACHE = `boobly-runtime-${VERSION}`;
 
@@ -8,12 +8,12 @@ const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.v1_4_3_Alpha.js",
+  "./app.v1_4_4_Alpha.js",
   "./database.json",
   "./prompts.json",
   "./changelog.json",
   "./presets.json",
-  "./master_file_v1_4_3_Alpha.json",
+  "./master_file_v1_4_4_Alpha.json",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
